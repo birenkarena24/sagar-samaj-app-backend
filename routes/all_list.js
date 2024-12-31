745ad6e9538b531ec589c80d7ea4f6ebb0d1fc64
birenkarena24 <birenkarena24@gmail.com>
2024-12-31 13:25:33 +0530

    added infinite scrolling pagination logic for all lists

--- routes/all_list.js
+++ routes/all_list.js
@@ -6,13 +6,25 @@ const listRouter = Router();
 
 // all list of chats
 listRouter.get("/all-chat", userMiddleware, async function(req, res){
+    const { loadedChats, requestChats } = req.body;
+    
     try{
         const chats = await ChatModel.find({
             isHidden: false
         })
+        .skip(parseInt(loadedChats))
+        .limit(parseInt(requestChats))
 
-        res.status(200).json(chats)
+        const hasMore = (await ChatModel.countDocuments({
+            isHidden: false
+        })) > parseInt(loadedChats) + chats.length
+
+        res.status(200).json({
+            chats,
+            hasMore
+        })
     } catch (err) {
+        console.log(err)
         res.status(500).json({
             msg: "internal server error"
         })
@@ -21,12 +33,16 @@ listRouter.get("/all-chat", userMiddleware, async function(req, res){
 
 // all list of boys of matrimony
 listRouter.get("/matrimony-boys", userMiddleware, async function(req, res){
+    const { loadedUsers, requestUsers } = req.body;
+    
     try{
         const boys = await UserModel.find({
             gender: "male",
             openToMarriage: true,
             isBlocked: false
         })
+        .skip(parseInt(loadedUsers))
+        .limit(parseInt(requestUsers))
 
         const boysList = boys.map(({firstName, lastName, profilePicUrl, dateOfBirth, address, occupation, jobRole, businessName, qualification}) => ({
             firstName,
@@ -40,7 +56,16 @@ listRouter.get("/matrimony-boys", userMiddleware, async function(req, res){
             qualification
         }))
 
-        res.status(200).json(boysList)
+        const hasMore = (await UserModel.countDocuments({
+            gender: "male",
+            openToMarriage: true,
+            isBlocked: false
+        })) > parseInt(loadedUsers) + boys.length
+        
+        res.status(200).json({
+            boysList,
+            hasMore,
+        })
     } catch (err) {
         console.log(err)
         res.status(500).json({
@@ -51,12 +76,16 @@ listRouter.get("/matrimony-boys", userMiddleware, async function(req, res){
 
 // all list of girls of matrimony
 listRouter.get("/matrimony-girls", async function(req, res){
+    const { loadedUsers, requestUsers } = req.body;
+    
     try{
         const girls = await UserModel.find({
             gender: "female",
             openToMarriage: true,
             isBlocked: false
         })
+        .skip(parseInt(loadedUsers))
+        .limit(parseInt(requestUsers))
 
         const girlsList = girls.map(({firstName, lastName, profilePicUrl, dateOfBirth, address, occupation, jobRole, qualification}) => ({
             firstName,
@@ -69,7 +98,16 @@ listRouter.get("/matrimony-girls", async function(req, res){
             qualification
         }))
 
-        res.status(200).json(girlsList)
+        const hasMore = (await UserModel.countDocuments({
+            gender: "female",
+            openToMarriage: true,
+            isBlocked: false
+        })) > parseInt(loadedUsers) + girls.length
+        
+        res.status(200).json({
+            girlsList,
+            hasMore,
+        })
     } catch (err) {
         console.log(err)
         res.status(500).json({
@@ -80,11 +118,15 @@ listRouter.get("/matrimony-girls", async function(req, res){
 
 // all list of members of community
 listRouter.get("/all-members", async function(req, res){
-    try{
+    const { loadedUsers, requestUsers } = req.body;
+
+    try {
         const members = await UserModel.find({
             isBlocked: false
         })
-
+        .skip(parseInt(loadedUsers))
+        .limit(parseInt(requestUsers))
+        
         const membersList = members.map(({firstName, lastName, profilePicUrl, address, occupation, jobRole, businessName, qualification, roleInSamaj}) => ({
             firstName,
             lastName,
@@ -96,8 +138,15 @@ listRouter.get("/all-members", async function(req, res){
             qualification,
             roleInSamaj
         }))
-
-        res.status(200).json(membersList)
+        
+        const hasMore = (await UserModel.countDocuments({
+            isBlocked: false
+        })) > parseInt(loadedUsers) + members.length
+        
+        res.status(200).json({
+            membersList,
+            hasMore,
+        })
     } catch (err) {
         console.log(err)
         res.status(500).json({
